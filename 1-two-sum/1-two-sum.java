08d65d90550f9d3b680240a03490e42e66f82e41
Plamen Savchev <58005043+Sav-age-chev@users.noreply.github.com>
2022-06-12 18:11:44 +0100

    Time: 1 ms (99.64%), Space: 42.5 MB (86.74%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,18 +1,33 @@
-//Optimised solution
+//Optimised solution 2 (Time Complexity: O(n), Space Compleity: O(n))
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map <Integer, Integer> map = new HashMap<>();
         for (int i=0; i<nums.length; i++) {
-            for (int k=i+1; k<nums.length; k++) {
-                if (nums[k] == target - nums[i]) {
-                    return new int[] {i, k};
-                }
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
             }
+            map.put(nums[i], i);
         }
         return null;
     }
 }
 
-//My initial solution
+//Optimised solution 1 (Time Complexity: O(n^2), Space Compleity: O(1))
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         for (int i=0; i<nums.length; i++) {
+//             for (int k=i+1; k<nums.length; k++) {
+//                 if (nums[k] == target - nums[i]) {
+//                     return new int[] {i, k};
+//                 }
+//             }
+//         }
+//         return null;
+//     }
+// }
+
+//My initial solution (Time Complexity: O(n^2), Space Compleity: O(1))
 // class Solution {
 //     public int[] twoSum(int[] nums, int target) {
         
@@ -31,6 +46,6 @@ class Solution {
 //                 }
 //             }
 //         }
-//         return output;
+//         return null;
 //     }
 // }
