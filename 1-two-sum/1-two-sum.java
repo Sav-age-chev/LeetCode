56b2a4c2e7a8464b360a1283ba8318e8081dc820
Plamen Savchev <58005043+Sav-age-chev@users.noreply.github.com>
2022-06-12 17:57:15 +0100

    Time: 130 ms (6.97%), Space: 44.6 MB (65.62%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,21 +1,36 @@
+//Optimised solution
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        int[] output = new int[2];
-        
         for (int i=0; i<nums.length; i++) {
-            output[0] = nums[i];
-            for (int k=0; k<nums.length; k++) {
-                if (k!=i) {
-                    output[1] = nums[k];
-                    if (output[0]+output[1]==target) {
-                        output[0] = i;
-                        output[1] = k;
-                        return output;
-                    } 
+            for (int k=i+1; k<nums.length; k++) {
+                if (nums[k] == target - nums[i]) {
+                    return new int[] {i, k};
                 }
             }
         }
-        return output;
+        return null;
     }
 }
+
+//My initial solution
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+        
+//         int[] output = new int[2];
+        
+//         for (int i=0; i<nums.length; i++) {
+//             output[0] = nums[i];
+//             for (int k=0; k<nums.length; k++) {
+//                 if (k!=i) {
+//                     output[1] = nums[k];
+//                     if (output[0]+output[1]==target) {
+//                         output[0] = i;
+//                         output[1] = k;
+//                         return output;
+//                     } 
+//                 }
+//             }
+//         }
+//         return output;
+//     }
+// }
