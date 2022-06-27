58ebd55dc6001313bd815eff50040331ac7864b2
Plamen Savchev <58005043+Sav-age-chev@users.noreply.github.com>
2022-06-27 23:24:42 +0100

    Time: 1 ms (34.91%), Space: 43 MB (38.47%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -2,7 +2,7 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-        // Decresing array length by 1 to be able to use the integer for index back to front
+        // Decreasing array length by 1 to be able to use the integer for index back to front
         m--;
         n--;
         
@@ -22,3 +22,25 @@ class Solution {
         }
     }
 }
+
+/* LeetCode optimised solution (Time complexity: O(n), Space complexity: O(1)) */
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         // Set p1 and p2 to point to the end of their respective arrays.
+//         int p1 = m - 1;
+//         int p2 = n - 1;
+        
+//         // And move p backwards through the array, each time writing
+//         // the smallest value pointed at by p1 or p2.
+//         for (int p = m + n - 1; p >= 0; p--) {
+//             if (p2 < 0) {
+//                 break;
+//             }
+//             if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+//                 nums1[p] = nums1[p1--];
+//             } else {
+//                 nums1[p] = nums2[p2--];
+//             }
+//         }
+//     }
+// }
