37b00b182967bd9be3db128b83cdae34841e54cb
Plamen Savchev <58005043+Sav-age-chev@users.noreply.github.com>
2022-06-28 23:51:56 +0100

    Time: 20 ms (29.18%), Space: 105.2 MB (22.85%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/* My initial solution (Time complexity: O(n), Space complexity: O(n)) */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        
+        // Initialising new array list
+        List<Integer> newList = new ArrayList<>();
+        
+        // Filling the array list with values from the linked list
+        while (head!=null) {
+            newList.add(head.val);
+            head = head.next;
+        }
+        
+        // Looping trough the array and camparing coresponding values 
+        for (int i=0; i<newList.size()/2; i++) {
+            // Return false if values doesn't match
+            if (newList.get(i)!=newList.get(newList.size()-1-i)) {
+                return false;
+            }
+        }
+        // Return true if the list is palindrome
+        return true;
+    }
+}
