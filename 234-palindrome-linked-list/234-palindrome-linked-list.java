7fe14dae10dd28749bb7df8f61a4926a74b0971b
Plamen Savchev <58005043+Sav-age-chev@users.noreply.github.com>
2022-06-29 01:07:54 +0100

    Time: 20 ms (29.18%), Space: 105.2 MB (22.85%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -33,3 +33,24 @@ class Solution {
         return true;
     }
 }
+
+/* LeetCode optimised solution (Time complexity: O(n), Space complexity: O(n)) */
+/* Recursive */
+// class Solution {
+
+//     private ListNode frontPointer;
+
+//     private boolean recursivelyCheck(ListNode currentNode) {
+//         if (currentNode != null) {
+//             if (!recursivelyCheck(currentNode.next)) return false;
+//             if (currentNode.val != frontPointer.val) return false;
+//             frontPointer = frontPointer.next;
+//         }
+//         return true;
+//     }
+
+//     public boolean isPalindrome(ListNode head) {
+//         frontPointer = head;
+//         return recursivelyCheck(head);
+//     }
+// }
